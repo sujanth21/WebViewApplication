a8f8c17ff1514a26447c6ef2f2d601e1abf12fe9
Sujanth <sujanth1987@gmail.com>
2017-08-25 08:16:03 +1000

    Web view loaded

--- WebViewApplication/ViewController.swift
+++ WebViewApplication/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let url = URL(string: "https://www.apple.com/au/")
+        let request = URLRequest(url: url!)
+        
+        webView.loadRequest(request)
     }
 
     override func didReceiveMemoryWarning() {
